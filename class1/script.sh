011fb336ccd7b331a96696fe4981f019518cc473
Aijan13 <13muratova@gmail.com>
2024-02-17 09:19:47 +0000

    folders

---
+++ class1/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo Hello world >> kaizen
+mkdir hello
+touch world
+useradd tim
+chmod 000 hello
+chown tim world
---
+++ class2/var.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+name=kaizen
+
+age=20
+
+city=chicago
+
+echo hello $name you are $age
+
+echo i live in $city
+
+echo $city is a great city
+
+echo $name live $city
+
+echo $age is underage
